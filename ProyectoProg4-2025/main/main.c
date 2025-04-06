6cba8cb89e806cc0dcb1e9ccf1439531494e1eb1
mariaMardones <115789955+mariaMardones@users.noreply.github.com>
2025-04-06 19:10:38 +0200

    main

--- ProyectoProg4-2025/main/main.c
+++ ProyectoProg4-2025/main/main.c
@@ -1,15 +1,37 @@
-#include "../bd/bd.h"
-#include "../domain/menus.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "menus.h"
+#include "bd.h"
 
 int main() {
     sqlite3 *db;
-    if (inicializar_bd(&db) != 0) {
+
+    if (!conectar_bd(&db)) {
         return 1;
     }
 
-    manejar_menu_principal(db);
+    crear_tablas(db); // solo crea "usuarios" de momento
+
+    int opcion;
+    do {
+        mostrar_menu_principal();
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                printf("Iniciar sesión (a implementar)...\n");
+                // Aquí iría validación y mostrar menú según rol
+                break;
+            case 2:
+                printf("Registro de usuario (a implementar)...\n");
+                break;
+            case 3:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opción no válida.\n");
+        }
+    } while (opcion != 3);
 
     cerrar_bd(db);
     return 0;
