90e108e49c5bb695f58876580a1ce292aa685db4
mariaMardones <115789955+mariaMardones@users.noreply.github.com>
2025-04-06 18:43:17 +0200

    main cliente

--- ProyectoProg4-2025/main/main.c
+++ ProyectoProg4-2025/main/main.c
@@ -1,68 +1,16 @@
-#include <stdio.h>
 #include "../bd/bd.h"
 #include "../domain/menus.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	printf("programa ejecutandose");
     sqlite3 *db;
-    if (inicializar_bd(&db) == 0) {
-        printf("Base de datos creada correctamente.\n");
-    } else {
-        printf("Error al inicializar la base de datos.\n");
+    if (inicializar_bd(&db) != 0) {
         return 1;
     }
 
-    int opcion;
-    char dni[20], contrasena[100], nombre[100];
-    int rol;
+    manejar_menu_principal(db);
 
-    while (1) {
-        mostrar_menu();  // Muestra el menú principal
-        scanf("%d", &opcion);
-
-        switch (opcion) {
-            case 1:  // Registrar
-                printf("Introduce nombre: ");
-                scanf("%s", nombre);
-                printf("Introduce DNI: ");
-                scanf("%s", dni);
-                printf("Introduce contraseña: ");
-                scanf("%s", contrasena);
-                printf("Introduce rol (0 = Cliente, 1 = Administrador): ");
-                scanf("%d", &rol);
-                if (registrar_usuario(db, nombre, dni, contrasena, rol) == 0) {
-                	printf("Usuario registrado con éxito.\n");
-                } else {
-                	printf("Error al registrar el usuario.\n");
-                }
-                break;
-
-            case 2:  // Iniciar sesión
-                printf("Introduce DNI: ");
-                scanf("%s", dni);
-                printf("Introduce contraseña: ");
-                scanf("%s", contrasena);
-
-                if (iniciar_sesion(db, dni, contrasena, &rol) == 0) {  // Iniciar sesión
-                    printf("Bienvenido, %s\n", dni);
-                    if (rol == 1) {
-                        menu_administrador();  // Si es admin, muestra menú administrador
-                    } else {
-                        menu_cliente();  // Si es cliente, muestra menú cliente
-                    }
-                } else {
-                    printf("Credenciales incorrectas.\n");
-                }
-                break;
-
-            case 3:  // Salir
-                cerrar_bd(db);  // Cierra la base de datos
-                return 0;
-
-            default:
-                printf("Opción no válida.\n");
-        }
-    }
-
+    cerrar_bd(db);
     return 0;
 }
