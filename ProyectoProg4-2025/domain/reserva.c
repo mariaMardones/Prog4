b35c9decfc005d0278ad063ad9a1af77eb2677ec
mariaMardones <115789955+mariaMardones@users.noreply.github.com>
2025-04-06 18:40:41 +0200

    cambios reserva

--- ProyectoProg4-2025/domain/reserva.c
+++ ProyectoProg4-2025/domain/reserva.c
@@ -1,14 +1,36 @@
 #include "reserva.h"
 #include <stdio.h>
-#include <string.h>
+#include <sqlite3.h>
 
-void inicializarReserva(Reserva *r, int codigo, int num_habitacion, const char *dni_usuario) {
-    r->codigo = codigo;
-    r->num_habitacion = num_habitacion;
-    strncpy(r->dni_usuario, dni_usuario, 9);
-    r->dni_usuario[9] = '\0';
+void hacer_reserva(sqlite3 *db, const char *dni, int habitacion, const char *fecha) {
+    char sql[256];
+    snprintf(sql, sizeof(sql),
+        "INSERT INTO Reserva (usuario_id, habitacion, fecha) "
+        "SELECT id, %d, '%s' FROM Usuario WHERE dni = '%s';",
+        habitacion, fecha, dni);
+    sqlite3_exec(db, sql, 0, 0, NULL);
 }
 
-void mostrarReserva(const Reserva *r) {
-    printf("Reserva %d - HabitaciÃ³n: %d - DNI: %s\n", r->codigo, r->num_habitacion, r->dni_usuario);
+void cancelar_reserva(sqlite3 *db, const char *dni, int habitacion) {
+    char sql[256];
+    snprintf(sql, sizeof(sql),
+        "DELETE FROM Reserva WHERE habitacion = %d AND usuario_id = (SELECT id FROM Usuario WHERE dni = '%s');",
+        habitacion, dni);
+    sqlite3_exec(db, sql, 0, 0, NULL);
+}
+
+void ver_reservas_activas(sqlite3 *db, const char *dni) {
+    const char *sql =
+        "SELECT R.id, R.habitacion, R.fecha FROM Reserva R "
+        "JOIN Usuario U ON R.usuario_id = U.id WHERE U.dni = ?;";
+    sqlite3_stmt *stmt;
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    sqlite3_bind_text(stmt, 1, dni, -1, SQLITE_STATIC);
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        printf("Reserva ID: %d | HabitaciÃ³n: %d | Fecha: %s\n",
+               sqlite3_column_int(stmt, 0),
+               sqlite3_column_int(stmt, 1),
+               sqlite3_column_text(stmt, 2));
+    }
+    sqlite3_finalize(stmt);
 }
--- ProyectoProg4-2025/domain/reserva.h
+++ ProyectoProg4-2025/domain/reserva.h
@@ -1,13 +1,15 @@
 #ifndef RESERVA_H
 #define RESERVA_H
-#include "habitacion.h"
 
 typedef struct {
-    int codigo;
+    int id;
     int num_habitacion;
     char dni_usuario[10];
+    char fecha[20];
 } Reserva;
 
-void inicializarReserva(Reserva *r, int codigo, int num_habitacion, const char *dni_usuario);
-void mostrarReserva(const Reserva *r);
+void hacer_reserva(sqlite3 *db, const char *dni, int habitacion, const char *fecha);
+void cancelar_reserva(sqlite3 *db, const char *dni, int habitacion);
+void ver_reservas_activas(sqlite3 *db, const char *dni);
+
 #endif
